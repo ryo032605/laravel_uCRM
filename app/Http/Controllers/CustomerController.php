c2382593278e27cf12229ccd533eed04a9c485ed
ryotanaka <larc.enable-53@outlook.com>
2022-12-11 15:08:41 +0900

    customers

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,6 +6,7 @@ use App\Models\Customer;
 use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
@@ -14,15 +15,15 @@ class CustomerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-       
-        
+       $customers = Customer::searchCustomers($request->search)
+       ->select('id', 'name', 'kana', 'tel')->paginate(50);
 
        
 
         return Inertia::render('Customers/Index', [
-            'customers' => Customer::select('id', 'name', 'kana', 'tel')->paginate(50)
+            'customers' => $customers
         ]);
     }
 
@@ -33,7 +34,7 @@ class CustomerController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Customers/Create');
     }
 
     /**
@@ -44,7 +45,22 @@ class CustomerController extends Controller
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        Customer::create([
+            'name' => $request->name,
+            'kana' => $request->kana,
+            'tel' => $request->tel,
+            'email' => $request->email,
+            'postcode' => $request->postcode,
+            'address' => $request->address,
+            'birthday' => $request->birthday,
+            'gender' => $request->gender,
+            'memo' => $request->memo,
+            ]);
+            return to_route('customers.index')
+            ->with([
+            'message' => '登録しました。',
+            'status' => 'success'
+            ]);
     }
 
     /**
