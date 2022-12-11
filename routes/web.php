4583d8622d06a2d25bc6b2ed502761a5decc6058
ryotanaka <larc.enable-53@outlook.com>
2022-12-11 14:04:12 +0900

    sec04_customers

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\InertiaTestController;
+use App\Http\Controllers\CustomerController;
 
 
 Route::resource('/items', ItemController::class)->middleware(['auth', 'verified']);
+
+Route::resource('customers', CustomerController::class)->middleware(['auth', 'verified']);
 
 Route::get('/inertia-test',function () {
         return Inertia::render('InertiaTest');
